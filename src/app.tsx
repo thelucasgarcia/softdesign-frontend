d22687ba2b0878750f9d5683225e49e194d5bb30
Lucas Garcia Santos <lucas.garcia@zup.com.br>
2022-06-07 18:43:07 -0300

    add workflow
    Signed-off-by: Lucas Garcia Santos <lucas.garcia@zup.com.br>

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import theme from "./config/theme";
 import { ComicProvider } from "./contexts/comics";
 import { FavoriteProvider } from "./contexts/favorites";
@@ -12,7 +12,7 @@ import app from "../package.json";
 const App: React.FC = () => {
   return (
     <React.Fragment>
-      <BrowserRouter basename={process.env.NODE_ENV !== "production" ? app.homepage : "/"}>
+      <HashRouter basename={process.env.NODE_ENV === "production" ? app.homepage : "/"}>
         <ThemeProvider theme={theme}>
           <ComicProvider>
             <FavoriteProvider>
@@ -21,7 +21,7 @@ const App: React.FC = () => {
             </FavoriteProvider>
           </ComicProvider>
         </ThemeProvider>
-      </BrowserRouter>
+      </HashRouter>
     </React.Fragment>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "https://thelucasgarcia.github.io/softdesign-frontend/" : "",
+  base: process.env.NODE_ENV === "production" ? "/softdesign-frontend/" : "",
   plugins: [react()]
 });
