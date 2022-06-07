9e4dab6e046e042365fd3389fa9e6393496c17bc
Lucas Garcia Santos <lucas.garcia@zup.com.br>
2022-06-07 19:02:01 -0300

    add workflow
    Signed-off-by: Lucas Garcia Santos <lucas.garcia@zup.com.br>

--- src/app.tsx
+++ src/app.tsx
@@ -7,12 +7,10 @@ import { FavoriteProvider } from "./contexts/favorites";
 import Navigation from "./routes/navigation";
 import GlobalStyle from "./styles/global";
 import "sweetalert2/dist/sweetalert2.css";
-import app from "../package.json";
-
 const App: React.FC = () => {
   return (
     <React.Fragment>
-      <HashRouter basename={process.env.NODE_ENV === "production" ? app.homepage : "/"}>
+      <HashRouter>
         <ThemeProvider theme={theme}>
           <ComicProvider>
             <FavoriteProvider>
